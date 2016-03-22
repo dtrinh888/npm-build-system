865a0027ecfaeead000142fe1f844e826bd271e1
dtrinh888 <dtrinh888@gmail.com>
2016-03-21 21:36:39 -0700

    webpack require and exports with CommonJS

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,21 @@ var path = require('path');
 var webpack = require('webpack');
 var packageData = require('./package.json');
 var filename = [packageData.name, packageData.version, 'js'];
- module.exports = {
+ 
+module.exports = {
  	entry: path.resolve(__dirname, packageData.main),
  	output: {
  		path: path.resolve(__dirname, 'build'),
  		filename: filename.join('.')
  	},
- 	devtool: 'source-map'
+ 	devtool: 'source-map',
+ 	module: {
+ 		loaders: [
+	 		{
+	 			test: /\.js$/,
+	 			exclude: /(node_modules)/,
+	 			loader: 'babel'
+	 		}
+ 		]
+ 	}
  };
